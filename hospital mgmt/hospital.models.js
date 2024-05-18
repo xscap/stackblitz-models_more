e732111244766d57520885c13a07e7f006732c75
Arnab Paul <86665762+xscap@users.noreply.github.com>
2024-05-18 09:29:38 +0530

    Update hospital.models.js

--- hospital mgmt/hospital.models.js
+++ hospital mgmt/hospital.models.js
@@ -1,5 +1,20 @@
 import mongoose from 'mongoose'
 
-const hospitalSchema = new mongoose.Schema({}, {timestamps: true})
+const hospitalSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+
+  address:{
+    type: String,
+    required: true
+  },
+
+  pincode:{
+    type: String
+  }
+  
+}, {timestamps: true})
 
 export const Hospitals = mongoose.model("Hospitals",hospitalSchema)
